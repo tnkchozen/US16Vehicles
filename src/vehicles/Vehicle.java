52fa351f136d36544b9d4329ca6b6ce1cd3416a9
Edmun <Edmun@LAPTOP-SFD2BHVN>
2019-02-27 15:48:38 -0500

    Add vehicle

---
+++ src/vehicles/Vehicle.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vehicles;
+
+/**
+ *
+ * @author Edmun
+ */
+public class Vehicle {
+    
+}
